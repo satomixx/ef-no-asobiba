cb9c619c4b32ed58f1723331e0027581a55e503d
Satomi Suyama <zymotic.zymosis@gmail.com>
2017-01-05 11:31:22 +0900

    move index to ground

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const AsyncAwaitPlugin = require("webpack-async-await") ;
 module.exports = {
     entry: {
         "main": "./src/js/main.js",
+        "ground": "./src/js/ground.js",
         "globe": "./src/js/globe.js"
     },
     output: {
